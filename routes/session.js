5a922b7eeda27c83b5e2282e2a2b9d69379828ea
crayola-eater <a-sheikh@outlook.com>
2020-11-09 22:56:01 +0000

    Created skeleton for session routes

---
+++ routes/session.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * TODO: need to create:
+ *      1. getAllSessions function in models/session.js
+ *      2. getAllSessions function in controllers/session.js
+ */
+router.get("/", () => null);
+
+/**
+ * TODO: need to create:
+ *      1. createSession function in models/session.js
+ *      2. createSession function in controllers/session.js
+ */
+router.post("/", () => null);
+
+/**
+ * TODO: need to create:
+ *      1. updateSession function in models/session.js
+ *      2. updateSession function controllers/session.js
+ */
+router.patch("/", () => null);
+
+module.exports = router;
