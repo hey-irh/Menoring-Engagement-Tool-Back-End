8b30a6fa0fbf40243a9b74721cb3637f4b5ba612
crayola-eater <a-sheikh@outlook.com>
2020-11-10 15:30:13 +0000

    Installed middleware for CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const sessionRouter = require("./routes/session");
@@ -11,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost.com" }));
 
 app.use("/", indexRouter);
 app.use("/sessions", sessionRouter);
