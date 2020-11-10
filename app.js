a568d25364476092e9e388d5f699e774482ea550
Isabel Holland <isabel@Isabels-MacBook-Air.local>
2020-11-10 16:00:46 +0000

    CORs added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost.com" }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/sessions", sessionRouter);
