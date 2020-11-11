4478964b2d71ece403494d30eb917967d25ab7a6
crayola-eater <a-sheikh@outlook.com>
2020-11-11 17:32:31 +0000

    Added updateSession function

--- Models/session.js
+++ Models/session.js
@@ -43,16 +43,15 @@ const createSession = async (newSession) => {
 // };
 
 const updateSession = async (sessionId, updates) => {
-
-  const {notes, menteeFeedback, mentorFeedback} = updates;
+  const { notes, menteeFeedback, mentorFeedback } = updates;
   const response = await query(
     `UPDATE session
-    SET (notes, mentee_feedback, mentor_feedback) = ($2, COALESCE(mentee_feedback, $3), COALESCE(mentor_feedback, $4))
-    WHERE id = $1 RETURNING *`, 
+    SET (notes, mentee_feedback, mentor_feedback) = ($2, COALESCE($3, mentee_feedback), COALESCE($4, mentor_feedback))
+    WHERE id = $1 RETURNING *;`,
     [sessionId, notes, menteeFeedback, mentorFeedback]
   );
   return response.rows;
-}
+};
 
 //similar async function
 
