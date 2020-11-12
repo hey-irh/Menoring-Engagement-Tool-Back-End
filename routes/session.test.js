a2f2f1e031d59fcec571e327179c060f9a23105d
crayola-eater <a-sheikh@outlook.com>
2020-11-12 19:56:26 +0000

    Wrote back end tests for GET, POST

--- routes/session.test.js
+++ routes/session.test.js
@@ -3,29 +3,29 @@ const supertest = require("supertest");
 const request = supertest(app);
 const assert = require("assert");
 
-describe("Tests for session routes", () => {
+const someSessionObject = expect.objectContaining({
+  id: expect.any(Number),
+  timestamp: expect.any(String),
+  notes: expect.arrayContaining([expect.any(String)]),
+  mentor_feedback: expect.anything(),
+  mentee_feedback: expect.anything(),
+  mentor_id: expect.any(Number),
+  mentee_id: expect.any(Number),
+});
+
+const someArrayOfSessionObjects = expect.arrayContaining([someSessionObject]);
+
+describe("GET /sessions", () => {
   it("should return an array of session objects", async (done) => {
-    const {
-      body: { payload },
-    } = await request.get("/sessions");
-    const actual =
-      Array.isArray(payload) &&
-      payload.every((session) =>
-        [
-          "id",
-          "timestamp",
-          "notes",
-          "mentor_feedback",
-          "mentee_feedback",
-          "mentor_id",
-          "mentee_id",
-        ].every((p) => session.hasOwnProperty(p))
-      );
-    expect(actual).toBe(true);
+    const response = await request.get("/sessions");
+    expect(response.body.payload).toStrictEqual(someArrayOfSessionObjects);
+    expect(response.status).toBe(200);
     done();
   });
+});
 
-  it("should return a session object", async (done) => {
+describe("POST /sessions", () => {
+  it("should return the newly created session object", async (done) => {
     const newSession = {
       notes: ["note 1", "note 2"],
       timestamp: new Date().toISOString(),
@@ -34,85 +34,45 @@ describe("Tests for session routes", () => {
       mentorId: 1,
       menteeId: 2,
     };
-    const {
-      body: { id, ...session },
-    } = await request.post("/sessions").send(newSession);
-
-    // const actual =
-    //   payload &&
-    //   typeof payload === "object" &&
-    //   [
-    //     "timestamp",
-    //     "notes",
-    //     "mentor_feedback",
-    //     "mentee_feedback",
-    //     "mentor_id",
-    //     "mentee_id",
-    //   ].every((p) => payload[p] === newSession[p]);
-    expect(session).toStrictEqual({
+    const response = await request.post("/sessions").send(newSession);
+    expect(response.body.payload).toMatchObject({
       notes: newSession.notes,
       timestamp: newSession.timestamp,
       mentor_feedback: newSession.mentor_feedback,
       mentee_feedback: newSession.mentee_feedback,
       mentor_id: newSession.mentorId,
-      mentee_id: newSession.mentorId,
+      mentee_id: newSession.menteeId,
     });
+    expect(response.status).toBe(200);
     done();
   });
 });
 
-// describe('POST /sessions', () => {
-//   it.only('responds with json', async (done) => {
-//     const response = await request.post('/sessions')
-//       .send( {
-//     timestamp: new Date(2020, 10, 1, 11),
-//     notes: [
-//       "Would be good if we could discuss promises at the next session. Doing them in workshops at the moment and I don't really understand them well yet.",
-//       "We can discuss a few examples, no problem.",
-//     ],
-//     mentorId: 1,
-//     menteeId: 2,
-//     mentorFeedback: null,
-//     menteeFeedback: 5,
-//   });
-//   // expect(response.contentType).toBe(json);
-//   expect(response.status).toBe(200);
+describe("PATCH /sessions/:id", () => {
+  it("should", async (done) => {
+    const sessionId = 1;
+    const updatedSession = {
+      notes: [
+        "Would be good if we could discuss promises at the next session. Doing them in workshops at the moment and I don't really understand them well yet.",
+        "We can discuss a few examples, no problem.",
+      ],
+      menteeFeedback: null,
+      mentorFeedback: 5,
+    };
 
-//   const jsonResponse = await response.json();
-//   expect(jsonResponse).toMatchObject({
-//     mentorId: 1
-//   });
-//   // .expect('Content-Type', /json/)
-//   // .expect(200)
-//   // .then(response => {
-//   //   assert(response === {
-//   //     timestamp: new Date(2020, 10, 1, 11),
-//   //     notes: [
-//   //       "Would be good if we could discuss promises at the next session. Doing them in workshops at the moment and I don't really understand them well yet.",
-//   //       "We can discuss a few examples, no problem.",
-//   //     ],
-//   //     mentorId: 1,
-//   //     menteeId: 2,
-//   //     mentorFeedback: null,
-//   //     menteeFeedback: 5,
-//   //   })
-//   // })
-//   //   done();
-// });
-// });
+    const response = await request
+      .patch(`/sessions/${sessionId}`)
+      .send(updatedSession);
 
-// describe('PATCH /sessions:id', () => {
-//   it('responds with json', async (done) => {
-//     const response = await request.patch('/sessions/1')
-//       .send( {
-//         sessionId: 1,
-//         notes:[
-//           "Would be good if we could discuss promises at the next session. Doing them in workshops at the moment and I don't really understand them well yet.",
-//           "We can discuss a few examples, no problem.",
-//         ],
-//         menteeFeedback: null,
-//         mentorFeedback: 5,
-//       })
-//       done();
-//   });
-// });
+    console.log(response.body.payload);
+
+    expect(response.body.payload).toMatchObject({
+      id: sessionId,
+      notes: updatedSession.notes,
+      mentee_feedback: updatedSession.menteeFeedback,
+      mentor_feedback: updatedSession.mentorFeedback,
+    });
+    expect(response.status).toBe(200);
+    done();
+  });
+});
